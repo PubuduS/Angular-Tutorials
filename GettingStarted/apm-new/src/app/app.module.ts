43e575f04676cdade4995b3d29dcb36a1b316b48
Wijesooriya <pubudu.wijesooriya@essvote.com>
2023-09-11 12:41:41 -0500

    Add the followings:
    Add Components
    Use 1 way Data Binding
    Use Interpolation
    Use Directives
    Use 2 way Data Binding
    Use Pipes to formatting

--- GettingStarted/apm-new/src/app/app.module.ts
+++ GettingStarted/apm-new/src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
