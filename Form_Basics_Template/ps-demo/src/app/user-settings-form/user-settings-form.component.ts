619fbc13755b322b40738cf763de2788e9cf0dd6
PubuduS <pubudu.wijesooriya@essvote.com>
2023-09-13 12:46:32 -0500

    Use ngForm
    Use ngModel
    Create interface for custom data
    Two way data binding
    Copying form data to avoid data corruption.

--- Form_Basics_Template/ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ Form_Basics_Template/ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.css']
 })
-export class UserSettingsFormComponent {
+export class UserSettingsFormComponent implements OnInit {
 
+  originalUserSettings : UserSettings = 
+  {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes'
+  };
+
+  userSettings : UserSettings =
+  {
+    ...this.originalUserSettings
+  };
+
+  constructor()
+  {}
+
+  ngOnInit(): void 
+  {    
+  }
 }
