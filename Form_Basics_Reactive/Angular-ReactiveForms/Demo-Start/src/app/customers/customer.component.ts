a3730627bba41198379191914404e8026030de77
PubuduS <pubudu.wijesooriya@essvote.com>
2023-09-15 11:09:27 -0500

    Add followings to reactive form.
    Use simple build in validations.
    Instead of template, moved the validation to component.
    
    Adjust validation rules at runtime. If user select email,
    phone field is optional, but if user select phone, phone field is required.
    
    Create custom validators.
    
    Create custom validator with multiple parameters.
    
    Cross-field validation.

--- Form_Basics_Reactive/Angular-ReactiveForms/Demo-Start/src/app/customers/customer.component.ts
+++ Form_Basics_Reactive/Angular-ReactiveForms/Demo-Start/src/app/customers/customer.component.ts
@@ -1,13 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
+
+// A normal validator function can only take 1 argument. If we need to pass more 
+// args, we need to wrap it in a factory function. We are doing this in below.
+// In this way, we can take many arguments and this function return a validator.
+function ratingRange( min: number, max: number ): ValidatorFn
+{
+  return ( c: AbstractControl ): { [key: string]: boolean } | null =>
+  {
+    if( c.value !== null && ( isNaN( c.value ) || c.value < min || c.value > max ) )
+    {
+      return { 'range': true };
+    }
+    return null;
+  }
+}
+
+function emailMatcher( c: AbstractControl ) : { [key: string] : boolean } | null
+{
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if( emailControl.pristine || confirmControl.pristine )
+  {
+    return null;
+  }
+
+  if( emailControl.value === confirmControl.value )
+  {
+    return null;
+  }
+
+  return { 'match': true };
+}
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
+
 export class CustomerComponent implements OnInit 
 {
 
@@ -23,9 +57,17 @@ export class CustomerComponent implements OnInit
     this.customerForm = this.fb.group
     (
         {
-          firstName: '',
-          lastName: '',
-          email: '',
+          firstName: [ '', [ Validators.required, Validators.minLength( 3 ) ] ],
+          lastName: [ '', [ Validators.required, Validators.maxLength( 50 ) ] ],
+
+          emailGroup: this.fb.group({
+            email: [ '', [ Validators.required, Validators.email ] ],
+            confirmEmail: [ '', [ Validators.required ] ],
+          }, { validator: emailMatcher } ),
+
+          phone: '',
+          notification: 'email',
+          rating: [ null, ratingRange( 1, 5 ) ], 
           sendCatalog: true
         }
     )
@@ -34,14 +76,34 @@ export class CustomerComponent implements OnInit
 
   populateTestData(): void
   {
-    this.customerForm.setValue(
+    this.customerForm.patchValue(
       {
         firstName: 'Jack',
         lastName: 'Harkness',
-        email: 'jack@gmail.com',
+        // email: 'jack@gmail.com',
+        //confirmEmail: 'jack@gmail.com',
+        emailGroup: { email: 'jack@gmail.com', confirmEmail: 'jack@gmail.com' },
+        phone: '',
+        notification: 'email',        
         sendCatalog: false
       }
     );
+  }
+
+  setNotification( notifyVia: string ): void
+  {
+    const phoneControl = this.customerForm.get('phone');
+
+    if( notifyVia === 'text' )
+    {
+      phoneControl.setValidators( Validators.required );
+    }
+    else
+    {
+      phoneControl.clearValidators();
+    }
+
+    phoneControl.updateValueAndValidity();
   }
 
   save(): void {
