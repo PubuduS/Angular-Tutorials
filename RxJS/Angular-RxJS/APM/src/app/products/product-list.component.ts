051edea00d1b6956cb29d5c06bf7cac214187f32
PubuduS <pubudu.wijesooriya@essvote.com>
2023-09-19 10:18:00 -0500

    Combined 2 data streams
    Combine product and category data and
    map the product id to category name.
    Now, instead of displaying category id
    we display the relevent category name in
    product list.

--- RxJS/Angular-RxJS/APM/src/app/products/product-list.component.ts
+++ RxJS/Angular-RxJS/APM/src/app/products/product-list.component.ts
@@ -1,36 +1,29 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Observable, catchError, of, EMPTY } from 'rxjs';
 
-import { Subscription } from 'rxjs';
-import { ProductCategory } from '../product-categories/product-category';
-
-import { Product } from './product';
 import { ProductService } from './product.service';
+import { ProductCategory } from '../product-categories/product-category';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent{
   pageTitle = 'Product List';
   errorMessage = '';
   categories: ProductCategory[] = [];
 
-  products: Product[] = [];
-  sub!: Subscription;
+  products$ = this.productService.productWithCategory$
+  .pipe(
+    catchError( err => { 
+      this.errorMessage = err;
+      return EMPTY;
+
+    })
+  );
 
   constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.sub = this.productService.getProducts()
-      .subscribe({
-        next: products => this.products = products,
-        error: err => this.errorMessage = err
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
 
   onAdd(): void {
     console.log('Not yet implemented');
