d025ea07ceef2cc4b3423ae7ee7aba764eb0acb1
PubuduS <pubudu.wijesooriya@essvote.com>
2023-09-21 10:39:01 -0500

    Angular ViewChild Decorator

--- Component_CommunicationT/Angular-Communication/APM/src/app/products/product-list.component.ts
+++ Component_CommunicationT/Angular-Communication/APM/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
@@ -10,28 +11,44 @@ import { ProductService } from './product.service';
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     showImage: boolean;
+    listFilter: string;
 
     imageWidth: number = 50;
     imageMargin: number = 2;
     errorMessage: string;
 
-    private _listFilter: string;
-
-    get listFilter(): string
-    {
-        return this._listFilter;
-    }
-
-    set listFilter( value: string )
-    {
-        this._listFilter = value;
-        this.performFilter( this.listFilter );
-    }
+    // The goal is to access native html property.
+    // To get an access we need to mark the html element with an id with #id format.
+    // We added #filterElement to html Input element in template and through that get access
+    @ViewChild('filterElement') filterElementRef : ElementRef;
+    @ViewChild(NgModel) filterInput: NgModel;
 
     filteredProducts: IProduct[];
     products: IProduct[];
 
-    constructor(private productService: ProductService) { }
+    constructor(private productService: ProductService) 
+    {        
+    }
+
+    // We can't access filter element from constructor because at that time componenet is not
+    // rendered yet. Therefore we need special lifecycle hook.
+    // this method will run after the view is rendered.
+    ngAfterViewInit(): void
+    {
+        // Through native element, we can access html properties such as focus.
+        // Native element can pose security risks. eg: cross site scripting attacks.
+        // Native element is tightly coupled to the browser.
+        // This can also bypass the data sanitation mechanism build into angular.
+        // Use as a last resort.
+        if( this.filterElementRef.nativeElement )
+        {
+            this.filterElementRef.nativeElement.focus();
+        }
+
+        this.filterInput.valueChanges.subscribe(
+            () => this.performFilter( this.listFilter )
+        );
+    }
 
     ngOnInit(): void {
         this.productService.getProducts().subscribe(
