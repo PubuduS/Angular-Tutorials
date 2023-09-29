7effe8a755ea0802e896ea10fdd276d1bdf65db0
PubuduS <pubudu.wijesooriya@essvote.com>
2023-09-29 16:09:46 -0500

    Add Lazy Loading
    We only need product routing if the user logged in.
    Therefore, we lazy load it after user logged in.
    
    Add preloading strategy to optimize performace.

--- Routing/Angular-Routing/APM/src/app/products/product.module.ts
+++ Routing/Angular-Routing/APM/src/app/products/product.module.ts
@@ -10,57 +10,48 @@ import { ProductEditInfoComponent } from './product-edit/product-edit-info.compo
 import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 import { ProductEditGuard } from './product-edit/product-edit.guard';
 
-import { AuthGuard } from '../user/auth.guard';
-
 import { SharedModule } from '../shared/shared.module';
 
 const ROUTES = [
-    { path: 'products', component: ProductListComponent },
-    { path: 'products/:id', component: ProductDetailComponent },
-    { path: 'products/:id/edit', component: ProductEditComponent }
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'products/:id/edit', component: ProductEditComponent }
 ];
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { 
-        path: 'products',
-        // canActivate: [AuthGuard],       
+      {
+        path: '',
+        component: ProductListComponent
+      },
+      {
+        path: ':id',
+        component: ProductDetailComponent,
+        resolve: { resolvedData: ProductResolver }
+      },
+      {
+        path: ':id/edit',
+        component: ProductEditComponent,
+        canDeactivate: [ProductEditGuard],
+        resolve: { resolvedData: ProductResolver },
         children: [
           {
             path: '',
-            component: ProductListComponent
+            redirectTo: 'info',
+            pathMatch: 'full'
           },
-          { 
-            path: ':id', 
-            component: ProductDetailComponent, 
-            resolve: { resolvedData: ProductResolver }
+          {
+            path: 'info',
+            component: ProductEditInfoComponent
           },
-          { 
-            path: ':id/edit', 
-            component: ProductEditComponent,
-            canDeactivate: [ProductEditGuard ],
-            resolve: { resolvedData: ProductResolver },
-            children: [
-              {
-                path: '',
-                redirectTo: 'info',
-                pathMatch: 'full'
-              },
-              {
-                path: 'info',
-                component: ProductEditInfoComponent
-              },
-              {
-                path: 'tags',
-                component: ProductEditTagsComponent
-              }
-            ]
+          {
+            path: 'tags',
+            component: ProductEditTagsComponent
           }
         ]
       }
-
     ])
   ],
   declarations: [
